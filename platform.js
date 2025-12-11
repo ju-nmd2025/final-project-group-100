6799f5b0a4c8cf00cdf17c103c4309b2382be62a
Simonchichi <dragonzimpan@gmail.com>
2025-12-11 13:15:26 +0100

    Made the platforms change color

--- platform.js
+++ platform.js
@@ -1,13 +1,17 @@
+const colors = ["red", "green", "blue", "yellow", "purple", "orange"];
+
 export default class Platform {
   constructor(x, y, w, h) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
+
+    this.color = random(colors); // Assign a random color from the array
   }
 
   draw() {
-    fill("blue");
+    fill(this.color);
     rect(this.x, this.y, this.w, this.h);
   }
 }
